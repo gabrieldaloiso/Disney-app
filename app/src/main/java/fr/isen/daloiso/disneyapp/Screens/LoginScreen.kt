de5509c9b05c0afa09042439ceb7629e33682d26
Alicia Bosch <alicia.bosch@isen.yncrea.fr>
2026-03-09 10:41:28 +0100

    LoginScreen

--- app/src/main/java/fr/isen/daloiso/disneyapp/Screens/LoginScreen.kt
+++ app/src/main/java/fr/isen/daloiso/disneyapp/Screens/LoginScreen.kt
@@ -1,2 +1,55 @@
 package fr.isen.daloiso.disneyapp.Screens
 
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import fr.isen.daloiso.disneyapp.components.BottomComponent
+import fr.isen.daloiso.disneyapp.components.HeadingTextComponent
+import fr.isen.daloiso.disneyapp.components.MyTextFieldComponent
+import fr.isen.daloiso.disneyapp.components.NormalTextComponent
+import fr.isen.daloiso.disneyapp.components.PasswordTextFieldComponent
+
+
+@Composable
+fun LoginScreen(navController: NavHostController) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(28.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column {
+                NormalTextComponent(value = "Hey, there")
+                HeadingTextComponent(value = "Welcome Back")
+            }
+            Spacer(modifier = Modifier.height(25.dp))
+            Column {
+                MyTextFieldComponent(labelValue = "Email", icon = Icons.Outlined.Email)
+                Spacer(modifier = Modifier.height(10.dp))
+                PasswordTextFieldComponent(labelValue = "Password", icon = Icons.Outlined.Lock)
+            }
+            BottomComponent(
+                textQuery = "Don't have an account? ",
+                textClickable = "Register",
+                action = "Login",
+                navController
+            )
+        }
+    }
+}
--- app/src/main/java/fr/isen/daloiso/disneyapp/Screens/ProfileScreen.kt
+++ app/src/main/java/fr/isen/daloiso/disneyapp/Screens/ProfileScreen.kt
@@ -1,4 +1,1 @@
 package fr.isen.daloiso.disneyapp.Screens
-
-class ProfileScreen {
-}
